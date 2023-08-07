eb311fe9e6fad38a5e77aa89ebabefdc8a591fc2
opulencedeveloper <victorkudos@gmail.com>
2023-08-07 19:29:32 +0100

    added font to portal

--- components/UI/Portal.js
+++ components/UI/Portal.js
@@ -1,9 +1,19 @@
 import { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  variable: "--font-poppins",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
 const Portal = (props) => {
   const [mounted, setMounted] = useState(false);
+
   const { children } = props;
+
   useEffect(() => {
     setMounted(true);
   }, []);
@@ -13,7 +23,7 @@ const Portal = (props) => {
   }
 
   return ReactDOM.createPortal(
-    <div>{children}</div>,
+    <div className={`${poppins.variable} font-poppins`}>{children}</div>,
     document.getElementById("navigation")
   );
 };
