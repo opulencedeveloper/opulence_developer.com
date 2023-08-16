4cac151cc1f1abc287a2a1f88945008c20fa5e1d
opulencedeveloper <victorkudos@gmail.com>
2023-08-16 22:05:34 +0100

    improved color

--- components/layouts/Main-Navigation.js
+++ components/layouts/Main-Navigation.js
@@ -63,7 +63,7 @@ const MainNavigation = () => {
           <div className="italic pl-12 text-[8.5px] md:pl-28 md:text-base">
             <Typewriter
               onInit={(typewriter) => {
-                typewriter.pauseFor(700)
+                typewriter.pauseFor(1000)
                   .typeString(
                     '<span className="italic">God gives the logic, I write the code</span>'
                   )
