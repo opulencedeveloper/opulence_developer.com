1e9f10e38f723eb75fba0d4827095935680741e0
opulencedeveloper <victorkudos@gmail.com>
2023-08-16 21:34:37 +0100

    improved color

--- components/layouts/Layout.js
+++ components/layouts/Layout.js
@@ -16,7 +16,7 @@ const Layout = (props) => {
     {activeLink === "/" && <div className="absolute bottom-0 right-0 left-0 top-0 z-20 bg-gradient-to-t from-transparent h-screen w-full"> <Image
             src="/images/hero.png"
             alt="Newspaper"
-            style={{ filter: 'brightness(0.1)' }}
+            style={{ filter: 'brightness(0.2)' }}
             className="h-full w-full object-cover"
             priority
             loading="eager"
