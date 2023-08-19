c9ab8a5a3e5f2f6d68a0e4c78b23ddc83ee13536
opulencedeveloper <victorkudos@gmail.com>
2023-08-19 16:03:25 +0100

    adjusted layout

--- components/about/SectionOne.js
+++ components/about/SectionOne.js
@@ -11,73 +11,73 @@ const SectionOne = () => {
     <p>I also create contents related to tech and design on <span className="underline font-semibold">Facebook</span> and <span className="underline font-semibold">YouTube</span>.</p>
     </div> */
   }
-  return <section className="py-8">
-    <h1 className="text-3xl font-bold mb-4">
-      My Name is Amobi Victor Chukwuka, I am a Full Softwaare Enginneer.
-    </h1>
-    {/* <p className="text-gray-500 mb-8">Published May 10, 2023</p> */}
-    <div className="flex flex-col sm:flex-row">
-      <div className=" mb-4 sm:mb-0 sm:mr-8">
-        <Image
-          src="/images/profile.jpg"
-          alt="Newspaper"
-          className=" border border-2 rounded"
-          width={600}
-          height={400}
-        />
+  return (
+    <section className="py-8">
+      <h1 className="text-3xl text-primary1 font-bold mb-4">
+        My Name is <span className="text-white">Amobi Victor Chukwuka,</span> I
+        am a <span className="text-white">Full Software Enginneer.</span>
+      </h1>
+      {/* <p className="text-gray-500 mb-8">Published May 10, 2023</p> */}
+      <div className="flex flex-col sm:flex-row">
+        <div className=" mb-4 sm:mb-0 sm:mr-8">
+          <Image
+            src="/images/profile.jpg"
+            alt="Newspaper"
+            className=" border border-2 rounded"
+            width={600}
+            height={400}
+          />
+        </div>
+        <div className="space-y-7">
+          <p className="text-lg mb-4">
+            Welcome! I'm delighted to have you here to share my programming
+            journey.
+          </p>
+          <p className="text-lg">
+            It all began at the age of 10 when I laid hands on my first
+            computer. The potential it held and the boundless opportunities it
+            offered fascinated me. This marked the inception of my passion for
+            programming, which I pursued academically and beyond.
+          </p>
+          <p className="text-lg">
+            In my pursuit of mastery, I embarked on online programming courses
+            and workshops. Devoting my time and energy, I delved into
+            cutting-edge programming languages and new skill sets. My expertise
+            spans embedded systems (Arduino), web development (HTML, CSS with
+            Tailwind CSS), React.js, mobile app development (Flutter), and
+            Node.js (Express.js), among others.
+          </p>
+          <p className="text-lg">
+            While still in school, I developed a platform where fellow students
+            accessed lecture notes, assignments, and solutions with ease, even
+            extending the service to various universities. This endeavor
+            showcased my abilities while providing value to others.
+          </p>
+          <p className="text-lg">
+            As I evolved, my appetite for knowledge and challenges grew.
+            Continuously expanding my programming prowess, I've transitioned
+            from coding for enjoyment to making it my profession over the past 5
+            years. My commitment is unwavering: dedicating my life to
+            programming and seizing every learning opportunity.
+          </p>
+          <p className="text-lg">
+            I eagerly seek challenges that spark curiosity, thought, adaptation,
+            growth, and learning. I hold the belief that each project is a
+            gateway to fresh insights and a chance to exhibit my skills. I am
+            confident in my ability to collaborate with you or contribute to
+            your endeavors, and I am excited about what we can achieve together.
+          </p>
+          <p className="text-lg">
+            Thank you for investing your time in exploring my programming
+            journey. Please feel free to explore my portfolio, where you can
+            find a selection of the projects I've undertaken. Should you need
+            assistance with debugging or wish to discuss a potential project,
+            don't hesitate to reach out.
+          </p>
+        </div>
       </div>
-      <div className="space-y-8">
-        <p className="text-lg mb-4">
-          Welcome! I'm thrilled to have you here and share my journey in the
-          world of programming with you.
-        </p>
-        <p className="text-lg">
-          It all started when I was just 10 years old and I got my hands on a
-          computer for the first time. I was fascinated by what it could do and
-          the endless possibilities it offered. This sparked my interest in
-          programming and led me to study it in school. But my passion didn't
-          stop there.
-        </p>
-        <p className="text-lg">
-          In my quest to become a better programmer, I attended online
-          programming classes and workshops, dedicating my time and energy to
-          learning the latest programming languages and acquiring new skills. I
-          honed my skills in embedded systems (Arduino), web development (HTML,
-          CSS (Tailwind CSS)), React.js, mobile application development
-          (Flutter), Node.js (Express.js), and much more.
-        </p>
-        <p className="text-lg">
-          When I was still in school, I developed a website where I posted
-          lecture notes, assignments, and solutions for my fellow students to
-          access with ease, covering some universities as well. This project
-          gave me the opportunity to showcase my skills and provide value to
-          others.
-        </p>
-        <p className="text-lg">
-          As my skills developed, I realized that I was hungry for more
-          knowledge and challenges. So, I continued to learn and grow my
-          expertise in programming. Over the past 5 years, I have been coding
-          for fun in private, and turned my hobby into a career. I am determined
-          to dedicate my life to programming and to keep expanding my knowledge
-          with every opportunity I get.
-        </p>
-        <p className="text-lg">
-          I am eager for new challenges that will make me wonder, think, adapt,
-          improve and learn new things. I believe that every project is an
-          opportunity to learn something new and to showcase my skills. I am
-          confident in my ability to work with you or for you and I look forward
-          to exploring new opportunities together.
-        </p>
-        <p className="text-lg">
-          Thank you for taking the time to read about me and my journey in
-          programming. Feel free to explore my portfolio and see some of the
-          projects that I have worked on. If you have any questions on flxing a
-          bug or would like to discuss a project, please don't hesitate to
-          contact me.
-        </p>
-      </div>
-    </div>
-  </section>;
+    </section>
+  );
 };
 
 export default SectionOne;
--- components/home/SectionThree.js
+++ components/home/SectionThree.js
@@ -1,129 +1,136 @@
 import Image from "next/image";
 import Socials from "../UI/Socials";
 
-
 const SectionThree = () => {
   const contents = [
     {
       name: "RESTful(Node.js)",
-      description: "This API exposes end points for CRUD opeartions, authentication and verification using Express.JS and MongoDB(Mongoose)",
-    imageUrl: "/images/api.jpg"
-    },{
+      description:
+        "This API exposes end points for CRUD opeartions, authentication and verification using Express.JS and MongoDB(Mongoose)",
+      imageUrl: "/images/api.jpg",
+    },
+    {
       name: "GraphQL(Node.js)",
-      description: "I developed a GraphQL project that efficiently retrieves necessary data from backend services. It provides a streamlined user experience and is more organized and easier to maintain.",
-    imageUrl: "/images/graphql.jpg"
+      description:
+        "I developed a GraphQL project that efficiently retrieves necessary data from backend services. It provides a streamlined user experience and is more organized and easier to maintain.",
+      imageUrl: "/images/graphql.jpg",
     },
     {
       name: "RESTful API(PHP)",
-      description: "This was written with raw php that exposes end points for simple email sign up and sign in.",
-      imageUrl: "/images/api.jpg"
+      description:
+        "This was written with raw php that exposes end points for simple email sign up and sign in.",
+      imageUrl: "/images/api.jpg",
     },
   ];
-  
- 
+
   return (
     <section className="px-5 mt-32 md:px-12">
-      <div >
+      <div>
         <p className="text-xl">Some Open Source Code</p>
         <div className="border-primary1 border-t-2 border-opacity-50 flex justify-center mt-5 items-start space-x-auto flex-wrap md:justify-evenly 2xl:justify-start space-x-2">
           {contents.map((content, index) => (
-            <div 
-            key={index}
-            className="w-80 py-2 space-y-2">
-             <div className="h-72 w-full rounded-tl-lg rounded-tr-lg overflow-hidden"><Image
+            <div key={index} className="w-80 py-2 space-y-2">
+              <div className="h-72 w-full rounded-tl-lg rounded-tr-lg overflow-hidden">
+                <Image
                   src={content.imageUrl}
                   alt="call a doctor logo"
                   className="h-full w-full"
                   width={540}
                   height={388}
-                /></div>   
-                    <p className="font-medium text-xl text-center">{content.name}</p>
-                   
-                 
-                <p className="p-auto text-center text-sm leading-tight">
+                />
+              </div>
+              <p className="font-medium text-xl text-center">{content.name}</p>
+
+              <p className="p-auto text-center text-sm leading-tight">
                 {content.description}
-                </p>
-                 <button className="flex items-center border-primary1 border-b-2 border-opacity-50 mx-auto pb-0.5">
-                    <p className="text-sm">Open in GitHub</p>{" "}
-                    <div className="ml-1">
-                      <Image
-                        src="/images/icon/arrow-right.svg"
-                        alt="arrow right icon"
-                        className="h-4 w-4 transform -rotate-45"
-                        width={16}
-                        height={16}
-                      />
-                    </div>
-                  </button> 
-                  
+              </p>
+              <button className="flex items-center border-primary1 border-b-2 border-opacity-50 mx-auto pb-0.5">
+                <p className="text-sm">Open in GitHub</p>{" "}
+                <div className="ml-1">
+                  <Image
+                    src="/images/icon/arrow-right.svg"
+                    alt="arrow right icon"
+                    className="h-4 w-4 transform -rotate-45"
+                    width={16}
+                    height={16}
+                  />
+                </div>
+              </button>
             </div>
           ))}
         </div>
       </div>
 
-
-
-
       <div className="mt-40 mb-28">
         <p className="text-xl md:text-2xl">Some brands I've worked with</p>
         <hr className="border-t-2 my-3 border-primary1 opacity-50" />
         <div className="space-x-auto flex space-x-3 h-32">
-         
-            <div className="h-24 w-52">
-                <Image
-                  src="/images/logo/best.svg"
-                  alt="call a doctor logo"
-                  className="h-full w-full"
-                  width={208}
-                  height={80}
-                />
-                  
-            </div>
-            <div className="h-24 w-52">
-                <Image
-                  src="/images/logo/optimum.svg"
-                  alt="call a doctor logo"
-                  className="h-full w-full"
-                  width={208}
-                  height={80}
-                />
-                  
-            </div>
-            <div className="h-24 w-52">
-                <Image
-                  src="/images/logo/ud.svg"
-                  alt="call a doctor logo"
-                  className="w-full h-full"
-                  width={208}
-                  height={80}
-                  
-                />
-                  
-            </div>
-            <div className="h-24 w-52">
-                <Image
-                  src="/images/call-a-doc.svg"
-                  alt="call a doctor logo"
-                  className="w-full h-full"
-                  width={208}
-                  height={80}
-                />
-                  
-            </div>
+          <div className="h-24 w-52">
+            <Image
+              src="/images/logo/best.svg"
+              alt="call a doctor logo"
+              className="h-full w-full"
+              width={208}
+              height={80}
+            />
+          </div>
+          <div className="h-24 w-52">
+            <Image
+              src="/images/logo/optimum.svg"
+              alt="call a doctor logo"
+              className="h-full w-full"
+              width={208}
+              height={80}
+            />
+          </div>
+          <div className="h-24 w-52">
+            <Image
+              src="/images/logo/ud.svg"
+              alt="call a doctor logo"
+              className="w-full h-full"
+              width={208}
+              height={80}
+            />
+          </div>
+          <div className="h-24 w-52">
+            <Image
+              src="/images/call-a-doc.svg"
+              alt="call a doctor logo"
+              className="w-full h-full"
+              width={208}
+              height={80}
+            />
+          </div>
         </div>
       </div>
 
       <div className="pb-10 space-y-5">
         <p className="text-lg">Let's build magic together</p>
         <hr className="border-t-2 my-3 border-primary1 opacity-50" />
-        <p className="leading-loose text-lg font-light">"I'm passionate about creating elegant, efficient solutions to complex problems and I'm excited to bring my skills to your next project. 
-        With experience in both front-end and back-end development, I'm comfortable working with a variety of programming languages and frameworks. From responsive mobile app. and web design to robust server architecture, I have the technical expertise to build scalable, secure systems that meet your needs.
-
-But I don't just focus on the code. As a software engineer, I understand the importance of collaboration, communication, and empathy. I work closely with stakeholders to ensure that I'm delivering the right solution, not just any solution. And I'm always eager to learn more about your business, your goals, and your users.
-
-Whether you're looking for a full-time employee, a part-time consultant, looking for someone to help you debug your code or a contract developer, I'm excited to hear about your project. So don't hesitate to reach out reach me on any of my social media handles - I'm looking forward to working with/for you.
-  and we can get started."</p>
-<button className="flex items-center bg-black px-5 py-3 text-white rounded-sm">
+        <p className="leading-loose text-lg font-light">
+          "I am deeply passionate about crafting elegant and efficient solutions
+          for intricate challenges, and I am thrilled at the opportunity to
+          apply my skills to your forthcoming project. With comprehensive
+          experience encompassing both front-end and back-end development, I
+          possess the flexibility to operate across a spectrum of programming
+          languages and frameworks. From designing responsive mobile
+          applications and websites to engineering robust server architectures,
+          I possess the technical prowess required to construct scalable and
+          secure systems tailored to your requirements. However, my focus
+          extends beyond mere coding. As a software engineer, I recognize the
+          significance of collaboration, effective communication, and empathy. I
+          collaborate closely with stakeholders to ensure that I deliver not
+          just any solution, but the right solution. Moreover, I possess an
+          unyielding enthusiasm to understand your business, objectives, and your
+          users on a deeper level. Whether you seek a dedicated full-time
+          professional, an adept part-time consultant, a troubleshooter for your
+          code, or a skilled contract developer, I am enthusiastic to learn
+          about your project. Please feel free to contact me through any of my
+          social media channels - I am eager to initiate our collaboration. Your
+          project holds great promise, and I am eagerly looking forward to
+          joining forces with you to embark on this journey."
+        </p>
+        <button className="flex items-center bg-black px-5 py-3 text-white rounded-sm">
           Send Message
           <Image
             src="/images/icon/arrow-right.svg"
@@ -134,8 +141,6 @@ Whether you're looking for a full-time employee, a part-time consultant, looking
           />
         </button>
       </div>
-
-     
     </section>
   );
 };
