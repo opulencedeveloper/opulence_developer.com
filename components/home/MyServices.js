4af7f20028bb519b787308052234f761e4f1cef5
opulencedeveloper <victorkudos@gmail.com>
2023-08-26 01:00:08 +0100

    finishing touches

--- components/home/MyServices.js
+++ components/home/MyServices.js
@@ -13,21 +13,32 @@ const MyServices = () => {
   });
   inView;
   return (
-    <section ref={ref} className="mt-24 mb-32 ">
-      <h2 className="text-3xl text-center font-medium md:text-4xl">My Services</h2>
+    <section ref={ref} className="mt-24 mb-32 space-y-10">
+      <h2 className="text-3xl text-center font-medium md:text-4xl">
+        My Services
+      </h2>
       <div
-        className={`flex flex-wrap justify-between items-center max-w-7xl mx-auto mt-7 px-5 lg:px-0`}
+        className={`flex flex-wrap justify-center items-center max-w-7xl mx-auto mt-7 px-5 lg:px-8 xl:px-0`}
       >
         {" "}
         {skills.map((skill, index) => (
-          <div key={index} className="flex-shrink-0 h-28 w-full overflow-hidden md:w-[30%]">
+          <div
+            key={index}
+            className="flex-shrink-0 h-28 w-full overflow-hidden mr-5 md:w-[30%]"
+          >
             {" "}
-            <div className={`flex items-center space-x-3 -mt-24  ${
+            <div
+              className={`flex items-center space-x-3 -mt-24  ${
                 inView ? "animate-slideFromTop" : ""
               }`}
             >
-              <div className="flex-shrink-0 h-16 w-16 bg-primary1 opacity-60"></div>
-              <p className="font-medium text-xl text-primary1 md:text-2xl">
+              <div className="flex items-center justify-center flex-shrink-0 h-16 w-16 border-[7px] border-primary1 border-opacity-60">
+                <div className="flex items-center justify-center flex-shrink-0 h-10 w-10 border-[5px] border-primary1 border-opacity-60">
+                  {" "}
+                  <div className="flex-shrink-0 h-7 w-7 border-[5px] border-primary1 border-opacity-60"></div>
+                </div>
+              </div>
+              <p className="font-medium text-xl text-primary1 lg:text-2xl">
                 {skill}
               </p>
             </div>{" "}
