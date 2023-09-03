811a4f364ef90ba3c5a64480fb442e151e3ba7bc
opulencedeveloper <victorkudos@gmail.com>
2023-09-03 20:42:42 +0100

    ready for production

--- components/home/MyServices.js
+++ components/home/MyServices.js
@@ -17,13 +17,13 @@ const MyServices = () => {
         My Services
       </h2>
       <div
-        className={`flex flex-wrap justify-center items-center max-w-7xl mx-auto mt-7 px-8 xl:px-0`}
+        className={`flex flex-col justify-center items-center max-w-4xl mx-auto mt-7 px-8 lg:justify-evenly lg:flex-row xl:px-0`}
       >
         {" "}
         {skills.map((skill, index) => (
           <div
             key={index}
-            className="flex-shrink-0 h-28 w-full overflow-hidden mr-5 md:w-[30%]"
+            className="flex-shrink-0 h-28 w-full overflow-hidden mr-5 md:w-64"
           >
             {" "}
             <div
