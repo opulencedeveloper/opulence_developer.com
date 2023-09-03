5a0262d51a9e72e4b7e42404bbc36b7d90a943ba
opulencedeveloper <victorkudos@gmail.com>
2023-09-03 10:43:11 +0100

    ready for production

--- components/home/SectionThree.js
+++ components/home/SectionThree.js
@@ -64,7 +64,7 @@ const SectionThree = () => {
     <section className="mt-32 ">
       <div>
         <p className="text-xl px-5 md:px-12 md:text-2xl">Some Open Source Code</p>
-        <div className="border-primary1 border-t-2 border-opacity-50 flex justify-center mt-5 items-start space-x-auto flex-wrap pt-5 md:pt-8 md:justify-evenly 2xl:justify-start space-x-2">
+        <div className="border-primary1 border-t-2 border-opacity-50 flex justify-center mt-5 items-start space-x-auto flex-wrap pt-5 px-5 md:px-12 md:pt-8 md:justify-evenly 2xl:justify-start space-x-2">
           {openSourceProjectInfo.map((openSourceProjectData, index) => (
             <div key={index} className="w-80 py-2 space-y-2">
               <div className="h-72 w-full rounded-tl-lg rounded-tr-lg overflow-hidden">
