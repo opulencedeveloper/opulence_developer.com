b9ec9604551e84a54333e0b890b6b909cadb1c10
opulencedeveloper <victorkudos@gmail.com>
2023-09-04 00:50:39 +0100

    ready for production

--- components/home/SuccessfulProjects.js
+++ components/home/SuccessfulProjects.js
@@ -22,7 +22,7 @@ const projects = [
     image: "hardware-projects-icon.png",
   },
   {
-    title: "Bugs Solved",
+    title: "Developers Bugs Solved",
     number: "212",
     alt: "Amobi Victor Hardware Bugs Solved Icon",
     image: "bugs-solved-icon.jpg",
