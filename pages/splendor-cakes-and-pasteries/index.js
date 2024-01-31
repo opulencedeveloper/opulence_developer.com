1e8031a913b3c36d1122e1937456327c07b18298
opulencdeveloper <victorkudos@gmail.com>
2024-01-31 10:08:48 +0100

    mercy-cuisine

--- pages/splendor-cakes-and-pasteries/index.js
+++ pages/splendor-cakes-and-pasteries/index.js
@@ -7,19 +7,25 @@ import ShopInfo from "@/components/shop/catering/ShopInfo";
 
 const whatsappNumber = "+2348133916916";
 
-const products = [
+const products = [ 
   { name: "Special bons", imageUrl: "special-bons.jpg" },
   { name: "Egg Roll", imageUrl: "egg-roll.jpg" },
+  { name: "Food Tray", imageUrl: "food-tray.jpg" },
   { name: "Chin Chin", imageUrl: "chin-chin.jpg" },
   { name: "Fruit cake", imageUrl: "fruit-cake.jpg" },
-  { name: "Donut", imageUrl: "donut.jpg" },
+  { name: "Donut", imageUrl: "donut.jpg" }, 
   { name: "Puf Puf", imageUrl: "puf-puf.jpg" },
+  { name: "Naked Cake", imageUrl: "naked-cake.jpg" },
   { name: "Special Meatpie", imageUrl: "special-meatpie.jpg" },
   { name: "Banana Bread", imageUrl: "banana-bread.jpg" },
+  { name: "Foil Cake", imageUrl: "foil-cake.jpg" },
+  { name: "All flavors Cake", imageUrl: "all-flavors.jpg" }, 
   { name: "Whipped Cream Iced Cake", imageUrl: "whipped-cream-iced-cake.jpg" },
   { name: "Butter Iced Cake", imageUrl: "butter-iced-cake.jpg" },
   { name: "Cup Cake", imageUrl: "cup-cake.jpg" },
+  { name: "Cake parfait", imageUrl: "cake-parfait.jpg" },
   { name: "Fondant Cake", imageUrl: "fondant-cake.jpg" },
+  { name: "Small Chops", imageUrl: "small-chops.jpg" },
   { name: "White Wedding Cakes", imageUrl: "white-wedding-cake.jpg" },
   {
     name: "Traditional Wedding Cakes",
