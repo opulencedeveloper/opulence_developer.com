c4863e9f981d1f2b18aed29657e4d92112273c89
opulencedeveloper <victorkudos@gmail.com>
2024-03-23 11:33:24 +0100

    theme change bug fix

--- components/homepage/Hero.js
+++ components/homepage/Hero.js
@@ -72,7 +72,7 @@ export default function Hero() {
       </div>
       <div
         ref={scroll}
-        className="absolute bottom-16 right-0 flex flex-col items-center justify-center space-y-8"
+        className="absolute bottom-24 right-0 flex flex-col items-center justify-center space-y-8"
       >
         <span className=" rotate-90 text-body-3">scroll</span>
         <div className="relative h-1 w-10 rotate-90 overflow-hidden">
