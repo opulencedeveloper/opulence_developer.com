c6aa2962eaa86bfd472856ed4b549e49e75827b8
Amobi Victor Chukwuka <victorkudos@gmail.com>
2024-09-20 14:56:49 +0100

    Update Contact.js

--- components/homepage/Contact.js
+++ components/homepage/Contact.js
@@ -237,7 +237,7 @@ export default function Contact() {
                 target="_blank"
                 rel="noreferrer"
               >
-                <Icon icon="mdi:twitter" color="#666" />
+                <Icon icon="mdi:facebook" color="#666" />
                 <div className="relative">
                   <span>Facebook</span>
                   <span className="absolute bottom-0 left-0 h-[0.10em] w-0 rounded-full bg-secondary-600 duration-300 ease-in-out group-hover:w-full"></span>
