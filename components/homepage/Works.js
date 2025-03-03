05285cd4d6543a90b987af157f25ecd0601d287e
opulencedeveloper <victorkudos@gmail.com>
2025-03-03 12:45:39 +0100

    updated

--- components/homepage/Works.js
+++ components/homepage/Works.js
@@ -9,6 +9,7 @@ export default function Works({ forwardedRef }) {
       className="nav-change overflow-hidden my-[10%]"
     >
      <Heading title="Projects" />
+      <span className="text-sm sm:text-lg text-secondary-600">Project Highlight</span>
       <div className="mt-10 grid grid-cols-1 gap-16 gap-y-10 md:grid-cols-12">
         {/* Project #1 */}
         <div className=" col-span-1 md:col-span-12">
@@ -17,44 +18,44 @@ export default function Works({ forwardedRef }) {
             img={'/assets/images/staging-jack.png'}
             alt="Staging Jack landing page"
             name="Staging Jack"
-            type="Web • Backend Development"
+            type="Fullstack • Backend and App. Development"
             year="2021"
-            tools="Node.js • TypeScript • MongoDB "
+            tools="Flutter • Node.js • TypeScript • MongoDB "
 
           />
         </div>
         {/* Project #2 */}
         <div className="col-span-1 pt-0 md:col-span-7 md:pt-16">
           <Projects
-            link="https://maureenportfoliowebsite.vercel.app"
-            img={'/assets/images/maureen-eugene.png'}
-            alt="Maureen eugene landing page"
-            name="Maureen Portfolio"
+            link="https://reicapitalgrowth.com/"
+            img={'/assets/images/rei-capital.png'} 
+            alt="REI capital growth"
+            name="REI capital growth"
             type="Web • Frontend Development"
             year="2021"
-            tools="Next.js • TailwindCSS"
+            tools="HTML • CSS • Javascript"
           />
         </div>
         <div className="col-span-1 pt-0 md:col-span-5 md:pt-80">
           <Projects
-            link="https://ologwusamuel.vercel.app"
-            img={'/assets/images/ologwu-samuel.png'}
-            alt="Ologwu Samuel portfolio"
-            name="Ologwu Samuel Portfolio"
+            link="https://aanbestedingsjurist.ai"
+            img={'/assets/images/aanbestedingsjurist.png'}
+            alt="Aanbestedingsjurist"
+            name="AI Assistant"
             type="Web • Frontend Development"
             year="2023"
-            tools="Next.js • TailwindCSS"
+            tools="HTML • TailwindCSS • Javascript"
           />
         </div>
         <div className="col-span-1 h-fit pt-0 md:col-span-8 md:pt-20">
           <Projects
-             link="https://www.janetmemorialhospital.com"
-             img={'/assets/images/janet-hospital.png'}
-             alt="Janet memorial hospital landing page"
-             name="Janet Hospital website"
+             link="https://contentmatch.io"
+             img={'/assets/images/contentmatch.png'}
+             alt="Contentmatch"
+             name="Contentmatch"
              type="Web • Frontend Development"
              year="2023"
-             tools="Next.js • TailwindCSS"
+            tools="HTML • TailwindCSS • Javascript"
           />
         </div>
         <div className="col-span-1 h-fit md:col-span-4">
@@ -66,6 +67,30 @@ export default function Works({ forwardedRef }) {
             type="Web • Backend Development"
             year="2023"
             tools="Node.js • Typescript • PostgreSql"
+          />
+         
+        </div>
+        <div className="col-span-1 h-fit md:col-span-7">
+        <Projects
+            link="https://uchecrypt.com"
+            img={'/assets/images/uchecrypt.png'}
+            alt="Uchecrypt website"
+            name="Uchecrypt"
+            type="Full stack • Backend and Front end Development"
+            year="2023"
+            tools="Next.js • TailwindCSS • Node.js • Typescript • MongoDb"
+          />
+         
+        </div>
+        <div className="col-span-1 h-fit md:col-span-5">
+        <Projects
+            link="https://www.ebocab.com"
+            img={'/assets/images/ebocab.png'}
+            alt="Ebocab website"
+            name="Ebocab"
+            type="Web • Front end Development"
+            year="2024"
+            tools="Next.js • Typescript • PostgreSql • Flutter"
           />
          
         </div>
